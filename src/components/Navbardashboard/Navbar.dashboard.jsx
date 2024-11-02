6584bd0ee007288817c21a3c69a53eb8dbc0bfa1
Your Name <Arkzero1235>
2024-11-02 22:20:26 +0700

    Dung

--- src/components/Navbardashboard/Navbar.dashboard.jsx
+++ src/components/Navbardashboard/Navbar.dashboard.jsx
@@ -5,7 +5,33 @@ function NavbarDashboard() {
     return (
         <>
             <div id={style["nav-container"]}>
-                
+                <p className={style["menu-Title"]}>MENU</p>
+                <ul className={style["nav-vertical"]}>
+                    <li>
+                        <p>Dashboard</p>
+                    </li>
+                    <li>
+                        <p>Sàn giao dịch</p>
+                    </li>
+                    <li>
+                        <p>Kho đồ</p>
+                    </li>
+                    <li>
+                        <p>Nạp tiền</p>
+                    </li>
+                    <li>
+                        <p>Thống kê giao dịch</p>
+                    </li>
+                    <li>
+                        <p>Gacha</p>
+                    </li>
+                    <li>
+                        <p>Tin nhắn</p>
+                    </li>
+                    <li>
+                        <p>Tài khoản của tôi</p>
+                    </li>
+                </ul>
             </div>
         </>
     )
